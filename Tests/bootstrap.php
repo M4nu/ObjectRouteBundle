ee6d2753f275d1d96106a48f21b27bff60f3db50
Emmanuel Vella <vella.emmanuel@gmail.com>
2012-07-07 00:39:22 +0300

    Update master

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
-$vendorDir = __DIR__ . '/../vendor';
-
-if (!@include($vendorDir . '/.composer/autoload.php')) {
+if (!@include(__DIR__ . '/../vendor/autoload.php')) {
     die("You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
 php composer.phar install
